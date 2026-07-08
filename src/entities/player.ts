41a61f91dcd962882cf698dab2071cea56090d78
Matt Dyer <madmatt1220@gmail.com>
2026-07-08 14:30:24 -0600

    working on player.ts

--- src/entities/player.ts
+++ src/entities/player.ts
@@ -6,33 +6,54 @@ export class Player {
   public vy = 0;
   public isInvulnerable = false;
   public jumpMultiplier = 1;
+  private _jumpStrength: number;
+  private _gravity: number;
 
   constructor(
     public x: number,
     public y: number,
     public width: number,
     public height: number,
-    private gravity: number,
-    private jumpStrength: number,
-    private speed: number
-  ) {}
+    gravity: number,
+    jumpStrength: number,
+    public speed: number
+  ) {
+    this._gravity = gravity;
+    this._jumpStrength = jumpStrength;
+  }
+
+  public get jumpStrength(): number {
+    return this._jumpStrength;
+  }
+
+  public set jumpStrength(value: number) {
+    this._jumpStrength = value;
+  }
+
+  public get gravity(): number {
+    return this._gravity;
+  }
+
+  public set gravity(value: number) {
+    this._gravity = value;
+  }
 
   public update(dt: number, directionX: number) {
     this.vx = updateHorizontalVelocity(this.vx, directionX, this.speed);
-    this.vy = applyGravity(this.vy, this.gravity);
+    this.vy = applyGravity(this.vy, this._gravity);
     
     this.x = updatePosition(this.x, this.vx, dt);
+
     this.y = updatePosition(this.y, this.vy, dt);
   }
 
   public jump() {
-    this.vy = applyJumpImpulse(this.vy, this.jumpStrength * this.jumpMultiplier);
+    this.vy = applyJumpImpulse(this.vy, this._jumpStrength * this.jumpMultiplier);
   }
 
   public setPowerup(type: string) {
     if (type === 'spring') {
       this.jumpMultiplier = 2;
-      // In a real game, we would reset this after some time/bounce
     } else if (type === 'shield') {
       this.isInvulnerable = true;
     }
